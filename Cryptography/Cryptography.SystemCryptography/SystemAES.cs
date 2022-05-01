0a0a308e0b55b23982cf4ed8006b0cdb5a2fe889
Lewis Edwards <lewis@9thcircle.net>
2022-05-01 20:03:49 +1000

    Started implementation of AES

--- Cryptography/Cryptography.SystemCryptography/SystemAES.cs
+++ Cryptography/Cryptography.SystemCryptography/SystemAES.cs
@@ -1,25 +1,53 @@
-﻿using Cryptography.Interfaces;
+﻿using System.IO;
+using System.Security.Cryptography;
+using Cryptography.Interfaces;
 
 namespace Cryptography.SystemCryptography
 {
     public class SystemAES : ISymmetricCipher
     {
-
         public byte[] generateKey()
         {
-            return null;
+            var rng = new RNGCryptoServiceProvider();
+            byte[] output = new byte[256 / 8];
+            rng.GetBytes(output);
+            return output;
         }
         public byte[] generateNonce()
         {
-            return null;
+            var rng = new RNGCryptoServiceProvider();
+            byte[] output = new byte[128 / 8];
+            rng.GetBytes(output);
+            return output;
         }
         public byte[] encrypt(byte[] data, byte[] key, byte[] nonce)
         {
-            return null;
+            var input = new MemoryStream(data);
+            var output = new MemoryStream();
+            var aes = new AesManaged();
+            var e = aes.CreateEncryptor();
+
+            using (CryptoStream cs = new CryptoStream(input, e, CryptoStreamMode.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(cs))
+                    sw.Write(data);
+                return output.ToArray();
+            }
+            
         }
         public byte[] decrypt(byte[] data, byte[] key, byte[] nonce)
         {
-            return null;
+            var input = new MemoryStream(data);
+            var output = new MemoryStream();
+            var aes = new AesManaged();
+            var d = aes.CreateDecryptor();
+
+            using (CryptoStream cs = new CryptoStream(input, d, CryptoStreamMode.Read))
+            {
+                using (StreamWriter sw = new StreamWriter(cs))
+                    sw.Write(data);
+                return output.ToArray();
+            }
         }
         public string primitiveName => "AES";
         public string primitiveVariation => "KeySize=256";
