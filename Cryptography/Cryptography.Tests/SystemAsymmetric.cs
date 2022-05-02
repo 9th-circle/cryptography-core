1ab7ab6e79218dbb16dd7a0ca0fe97a4a6794223
Lewis Edwards <lewis@9thcircle.net>
2022-05-02 11:11:35 +1000

    RSA test working

---
+++ Cryptography/Cryptography.Tests/SystemAsymmetric.cs
@@ -0,0 +1,26 @@
+﻿using Cryptography.SystemCryptography;
+using Xunit;
+
+namespace Cryptography.Tests
+{
+    public class SystemAsymmetric
+    {
+
+        [Fact]
+        public void basicRSA()
+        {
+            byte[] data = new byte[64];
+            for (int i = 0; i < data.Length; i++)
+                data[i] = (byte)i;
+
+            var rsa = new SystemRSA();
+            var keys = rsa.generateKeyPair();
+
+            var encrypted = rsa.encrypt(data, keys.publicKey);
+            var decrypted = rsa.decrypt(encrypted, keys.privateKey);
+
+            Assert.NotEqual(encrypted, decrypted);
+            Assert.Equal(data, decrypted);
+        }
+    }
+}
