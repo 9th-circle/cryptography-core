bf2cffb5c5a0ee5c908accc27e60997641a2e4c1
Lewis Edwards <lewis@9thcircle.net>
2022-05-13 15:16:24 +1000

    Added length prefixed hash primitive along with existing hash tree construction classes

---
+++ Cryptography/Cryptography.Core/HashTrees/LengthPrefixedHash.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cryptography.Interfaces;
+
+namespace Cryptography.Core.HashTrees
+{
+    class LengthPrefixedHash : IHash
+    {
+        IHash inner;
+        public LengthPrefixedHash(IHash inner)
+        {
+            this.inner = inner;
+        }
+        public byte[] hash(byte[] input)
+        {
+            List<byte> data = new List<byte>();
+            data.AddRange(BitConverter.GetBytes(input.Length));
+            data.AddRange(input);
+            return inner.hash(data.ToArray());
+        }
+        public string primitiveName => inner.primitiveName;
+        public string primitiveVariation => inner.primitiveVariation;
+        public string implementationName => inner.implementationName;
+    }
+}
---
+++ Cryptography/Cryptography.Core/HashTrees/MerkleHashTree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cryptography.Interfaces;
+
+namespace Cryptography.Core.HashTrees
+{
+    class MerkleHashTree :IHashTree
+    {
+    }
+}
